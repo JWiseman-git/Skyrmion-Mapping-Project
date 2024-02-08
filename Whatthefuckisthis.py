1e1f158bd5b049e169aaa3cc80b5b81d9b6b5fb5
Jordan Wiseman <74243511+JWiseman-git@users.noreply.github.com>
2024-02-08 15:49:49 -0500

    Delete Whatthefuckisthis.py

--- Whatthefuckisthis.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x = np.arange(-5, 5, 1)
-y = np.arange(-5, 5, 1)
-xx, yy = np.meshgrid(x, y, sparse=True)
-z = np.sin(xx**2 + yy**2) / (xx**2 + yy**2)
-h = plt.contourf(x,y,z)
-plt.show()
